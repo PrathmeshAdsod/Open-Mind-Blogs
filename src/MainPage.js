f02e13840c4dbb7e3f65f02b12e90f9b221a10db
Prathmesh Adsod <prathmeshadsod@gmail.com>
2023-06-27 13:26:53 +0530

    header changed

--- src/MainPage.js
+++ src/MainPage.js
@@ -5,7 +5,14 @@ import './css/MainPage.css'
 function MainPage() {
   return (
     <>
-       <p className='title-of-blog'>Open Work</p>
+       <div className='header'>
+           <img className='logo' src='/open Work-logos_white.png' alt='logo'/>
+           <div className='tag'>
+              <p className='tagline'>Unlimited Stories, Novels, Information, Ideas for free.</p>
+              <p className='slogan'>Creativity within you!</p>
+           </div>
+       </div>
+       
 
        <div className='all-contents'>
             
