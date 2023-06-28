70ab45f49df742c6e477c5baf4b77921436fb185
Prathmesh Adsod <prathmeshadsod@gmail.com>
2023-06-28 23:47:53 +0530

    slogan changed

--- src/MainPage.js
+++ src/MainPage.js
@@ -8,14 +8,17 @@ function MainPage() {
        <div className='header'>
            <img className='logo' src='/open Work-logos_white.png' alt='logo'/>
            <div className='tag'>
-              <p className='tagline'>Unlimited Stories, Novels, Information, Ideas for free.</p>
-              <p className='slogan'>Creativity within you!</p>
+              <p className='tagline'>Unlimited Stories, Novels, Adventures, Information, Ideas, Motivation for free.</p>
+              <p className='slogan'>Think Creative, Think Bold</p>
            </div>
        </div>
        
 
        <div className='all-contents'>
-            
+            <div className='Novels'>
+                <p></p>
+                <div></div>
+            </div>
        </div>
     
     </>
